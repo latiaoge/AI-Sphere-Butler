a8a29aed2f0ae92e710081e0c02a8a7a51564223
玩电脑的辣条哥 <93706945+latiaoge@users.noreply.github.com>
2025-08-30 12:41:06 +0800

    Delete core/client/web/realtalk directory

--- core/client/web/realtalk/examples/pcm.js
+++
@@ -1,6 +0,0 @@
-async function convertToPCM(arrayBuffer) {
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
-    const pcmData = audioBuffer.getChannelData(0); // 获取单声道数据
-    return new Float32Array(pcmData);
-}
